fe843ecf6ac96930ae6a21ec35b4874e1b21be14
marush-in <shin.marumoto@gmail.com>
2019-10-03 10:19:41 +0900

    Step 3: データベースを作成する

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from contextlib import closing
 
 
 DATABASE = 'tmp/flaskr.db'
@@ -16,6 +17,12 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 if __name__ == '__main__':
